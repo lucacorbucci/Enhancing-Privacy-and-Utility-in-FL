673c3555248d2476684a7f55632d7bcf9a300b30
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-03-01 21:47:15 +0100

    Creates configure_preferences function to switch between json and python dictionary experiment configuraiton.

---
+++ pistacchio_light/Utils/configure_preferences.py
@@ -0,0 +1,5 @@
+def configure_preferences(path) -> dict:
+    """This function accepts the json preferences object and transforms it into
+    preferences dicitonary. Using this function, we can easily convert between running
+    the experiment from the json configuration or python dictionary. """
+    pass
