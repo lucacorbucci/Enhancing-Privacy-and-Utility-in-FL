b40a67ab9c801f2fe83059d05eeedb287508935e
Luca Corbucci <corbuccilu@gmail.com>
2023-09-05 10:16:47 +0000

    ✨ Improved the clarity of the preferences class using Pydantic

--- pistacchio_simulator/Utils/preferences.py
+++ pistacchio_simulator/Utils/preferences.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from pydantic import BaseModel, Field  # , field_validator
+from pydantic.dataclasses import dataclass
 
 
 class PartitionConfig(BaseModel):
@@ -12,38 +13,50 @@ class PartitionConfig(BaseModel):
     alpha: Optional[int] = None
     percentage_configuration: Optional[dict] = None
     store_path: str = Field(...)
+    server_validation_set: str = Field(...)
+
 
 class P2PConfig(BaseModel):
     fl_rounds: int
     local_training_epochs: int
     round_mixed_mode: Optional[int] = 0
-    differential_privacy: bool 
+    differential_privacy: bool
     differential_privacy_mixed_mode: Optional[bool] = False
     epsilon: Optional[float] = None
-    noise_multiplier: Optional[float] = None 
-    delta: Optional[float] = None
+    noise_multiplier: Optional[float] = None
     epsilon_mixed: Optional[float] = None
+
+
 class ServerConfig(BaseModel):
-    fl_rounds = int
-    local_training_epochs = int
+    fl_rounds: int = Field(...)
+    local_training_epochs: int
     differential_privacy: bool
-    mixed_model: bool 
-    FL_rounds: int 
+    mixed_mode: bool
     total_mixed_rounds: Optional[int] = 0
     epsilon: Optional[float] = None
-    noise_multiplier: Optional[float] = None 
-    delta: Optional[float] = None
+    noise_multiplier: Optional[float] = None
+
+
 class HyperparametersConfig(BaseModel):
     batch_size: int
     lr: float
-    max_phisical_batch_size: int 
+    max_phisical_batch_size: int
+    delta: Optional[float] = None
+    max_grad_norm: float = Field(...)
+
+
 class WandbConfig(BaseModel):
     tags: list[str]
-    name: str 
+    name: str
     project_name: str
 
-class Preferences(BaseModel):
+
+@dataclass
+class Preferences:
     dataset: str = Field(...)
+    task_type: str = Field(...)
+    debug: bool = Field(...)
+    wandb: bool = Field(...)
     public_private_experiment: bool = Field(...)
     pool_size: int = Field(...)
     data_split_config: PartitionConfig = Field(...)
@@ -51,4 +64,4 @@ class Preferences(BaseModel):
     server_config: Optional[ServerConfig] = None
     hyperparameters_config: Optional[HyperparametersConfig] = Field(...)
     gpu_config: list[str] = Field(...)
-    wandb_config = Optional[WandbConfig] = Field(...)
+    wandb_config: Optional[WandbConfig] = None
